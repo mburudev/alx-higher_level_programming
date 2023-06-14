521a5bc88162cf4898382484f7edec0b46ab5273
mburudev <ianmburu999@gmail.com>
2023-06-14 12:25:18 -0700

    Task 14 correction 1

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i%2 == 0:
-        print(chr(i), end="")
-    else:
-        print(chr(i - 32), end="")
+for letter in range(122, 64, -1):
+    print(f"{chr(letter)}{chr(letter-32)}", end="")
