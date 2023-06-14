e66c4a0316ef6f59ec8de470f2de5d6ff9493097
mburudev <ianmburu999@gmail.com>
2023-06-14 12:39:04 -0700

    Task 15 correction 1

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    new_string = str[:n] + str[n+1:]
-    return (new_string)
+    if n > 0:
+        new_string = str[:n] + str[n+1:]
+        return (new_string)
+    else:
+        return(str)
