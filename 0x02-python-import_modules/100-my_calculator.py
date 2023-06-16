4458370b3ad6d589530f2636d3e3e3a025905cf8
mburudev <ianmburu999@gmail.com>
2023-06-16 08:59:45 -0700

    Task 6 correction 1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,12 +12,12 @@ if __name__ == "__main__":
         operator = sys.argv[2]
         b = int(sys.argv[3])
     if operator == "+":
-        print(add(a, b))
+        print("{} + {} = {}".format(a, b, add(a, b)))
     elif operator == "-":
-        print(sub(a, b))
+        print("{} - {} = {}".format(a, b, sub(a, b)))
     elif operator == "*":
-        print(mul(a, b))
+        print("{} * {} = {}".format(a, b, mul(a, b)))
     elif operator == "/":
-        print(div(a, b))
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
