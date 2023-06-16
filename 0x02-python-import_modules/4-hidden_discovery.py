85416cd9f0dec7a91ed989bd3ba1155a57498998
mburudev <ianmburu999@gmail.com>
2023-06-16 07:59:19 -0700

    Adhered to pycodestyle

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     import hidden_4
 
     module_names = dir(hidden_4)
-    filtered_strings = [name for name in module_names if isinstance(name, str) and '_' in name and not name.startswith('__')] 
+    filtered_strings = [name for name in module_names
+                        if isinstance(name, str) and '_' in name
+                        and not name.startswith('__')]
     for string in filtered_strings:
         print("{}".format(string))
