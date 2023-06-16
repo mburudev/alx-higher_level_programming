09a7f8f4475c93fd50b7db4e69cd87cc6b20f8c3
mburudev <ianmburu999@gmail.com>
2023-06-16 09:13:06 -0700

    Task 6 correction 2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
 
     args = len(sys.argv) - 1
 
-    if args == 0:
+    if args != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     i = 0
