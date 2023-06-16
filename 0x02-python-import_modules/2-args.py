aaf3a4a0ae4f8d538d05ce4f2782829e44dd1be0
mburudev <ianmburu999@gmail.com>
2023-06-16 06:21:41 -0700

    Task 2 correction 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-args = len(sys.argv) - 1
-if args < 1:
-    print("{} arguments".format(args))
-elif args == 1:
-    print("{} argument".format(args))
-else:
-    print("{} arguments:".format(args))
-for i in range(args):
-    print("{}: {}".format(i+1, sys.argv[i+1]))
+    args = len(sys.argv) - 1
+
+    if args < 1:
+        print("{} arguments".format(args))
+    elif args == 1:
+        print("{} argument".format(args))
+    else:
+        print("{} arguments:".format(args))
+        for i in range(args):
+            print("{}: {}".format(i+1, sys.argv[i+1]))
