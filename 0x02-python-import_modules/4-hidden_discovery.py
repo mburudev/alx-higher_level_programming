e0b88f911575c32c36fe4732e53961a744f0c3ee
mburudev <ianmburu999@gmail.com>
2023-06-16 07:36:29 -0700

    Task 4 correction 2

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,3 +3,7 @@
 if __name__ == "__main__":
     import hidden_4
 
+    module_names = dir(hidden_4)
+    filtered_strings = [name for name in module_names if isinstance(name, str) and '_' in name]
+    for string in filtered_strings:
+        print("{}".format(string))
