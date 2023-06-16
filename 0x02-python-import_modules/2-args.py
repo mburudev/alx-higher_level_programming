8709fc0d1ca29cbdf7a260f8a92603aef8f42600
mburudev <ianmburu999@gmail.com>
2023-06-16 06:18:04 -0700

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+args = len(sys.argv) - 1
+if args < 1:
+    print("{} arguments".format(args))
+elif args == 1:
+    print("{} argument".format(args))
+else:
+    print("{} arguments:".format(args))
+for i in range(args):
+    print("{}: {}".format(i+1, sys.argv[i+1]))
