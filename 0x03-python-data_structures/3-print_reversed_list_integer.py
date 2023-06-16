81ff9928a52bd1d6ae01be3a833ab9b79a8833e2
mburudev <ianmburu999@gmail.com>
2023-06-16 09:54:58 -0700

    Task 3 correction 1

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     reversed_list = f"{my_list[::-1]}"
     for i in reversed_list:
         if i != ',' and i != '[' and i != ']' and i != ' ':
-            print(i)
+            print("{}".format(i))
