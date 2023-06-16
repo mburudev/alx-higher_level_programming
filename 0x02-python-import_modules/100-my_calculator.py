5a61e464e169a26d16031f6885649997cbca2dc9
mburudev <ianmburu999@gmail.com>
2023-06-16 08:53:03 -0700

    Task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import calculator_1
+    from calculator_1 import add, sub, div, mul
+    import sys
+
+    args = len(sys.argv) - 1
+
+    i = 0
+    for i in range(args):
+        a = int(sys.argv[1])
+        operator = sys.argv[2]
+        b = int(sys.argv[3])
+    if operator == "+":
+        print(add(a, b))
+    elif operator == "-":
+        print(sub(a, b))
+    elif operator == "*":
+        print(mul(a, b))
+    elif operator == "/":
+        print(div(a, b))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
