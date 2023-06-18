921033f72cd7ab0649c99b5406e6cce102e9a212
mburudev <ianmburu999@gmail.com>
2023-06-18 01:48:49 -0700

    Task 13 correction

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None:
-        return 0
+    total_num = 0
+    total_den = 0
+    for i in my_list:
+        total_num += int(i[0] * i[1])
+        total_den += i[1]
+    return total_num/total_den
