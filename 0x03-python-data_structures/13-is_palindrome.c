17f64e8c2ca5b3670bfcd3a7cdec65a71f64dee4
mburudev <ianmburu999@gmail.com>
2023-06-23 06:19:21 -0700

    Task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome
+ * @head: Double pointer to the head of the linked list
+ *
+ * Return: 1 if the linked list is a palindrome, 0 otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+    /* Empty list is considered a palindrome */
+	if (*head == NULL)
+		return (1);
+	/* Find the middle node using two pointers (slow and fast) */
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *prev = NULL;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		listint_t *next = slow->next;
+
+		slow->next = prev;
+		prev = slow;
+		slow = next;
+	}
+	/* If the list has an odd number of nodes, move slow to the next node */
+	if (fast != NULL)
+		slow = slow->next;
+	/* Compare the first half (prev) with the second half (slow) */
+	while (prev != NULL && slow != NULL)
+	{
+		if (prev->n != slow->n)
+			return (0);
+		prev = prev->next;
+		slow = slow->next;
+	}
+	return (1); /* It's a palindrome */
+}
