79d595e3a05dc154fcd0283f6ca24b9dbd29449c
mburudev <ianmburu999@gmail.com>
2023-07-15 09:26:53 -0700

    Task 0 correction 1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,25 @@
-#!/usr/bin/python3
+"""
+This module provides functions for reading and printing the contents of a text file.
+
+Author: Ian Mburu
+"""
+
 def read_file(filename=""):
+    """
+    Read the contents of a text file and print them to stdout.
+
+    Args:
+        filename (str): The name of the text file to be read. Default is an empty string.
+
+    Returns:
+        None
+
+    Raises:
+        FileNotFoundError: If the specified file is not found.
+
+    Example:
+        read_file("example.txt")
+    """
     with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
             print(line, end='')
