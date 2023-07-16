10394c200819207d0f686ee6ef4d664629ae2837
mburudev <ianmburu999@gmail.com>
2023-07-16 09:37:39 -0700

    Task 13 adhered to PEP8

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,7 +6,7 @@ Provides functions for appending text in case of a specific string.
 
 def append_after(filename="", search_string="", new_string=""):
     """
-    Inserts a line of text after each line containing a specific string in a file.
+    Inserts a line of text after each line containing a specific string.
 
     Args:
         filename (str): The name of the file.
