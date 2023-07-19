e5c5068a9893958e47103226d2461026eb581737
mburudev <ianmburu999@gmail.com>
2023-07-19 07:21:18 -0700

    Task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Provides is_kind_of_class functions"""
+
+
+def is_kind_of_class(obj, a_class):
+    """function: is_kind_of_class
+    obj: an object
+    arg: a class
+    Returns: Bool
+    """
+    return isinstance(obj, a_class)
