7c4f99bc7abc32b78961ec3378d0fc9ee7fb5c45
mburudev <ianmburu999@gmail.com>
2023-07-19 07:54:12 -0700

    Task 4"

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Provides inherits_from function"""
+
+
+def inherits_from(obj, a_class):
+    """
+    the object is an instance of a class that inherited (directly or indirectly)
+    obj: an object
+    a_class: a class
+    returns None
+    """
+    return type(obj) != a_class and isinstance(obj, a_class)
