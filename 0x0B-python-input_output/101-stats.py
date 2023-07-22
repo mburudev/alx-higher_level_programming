60513891c3cde15386f02e74e7b96012106b3b6c
mburudev <ianmburu999@gmail.com>
2023-07-22 07:58:30 -0700

    Task 14 correction 5

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
                 codes[code] += 1
         except ValueError as e:
             # Handle the ValueError (could not convert to integer)
-            print()
+            sys.stdout.write('')
         except IndexError as e:
             # Handle the IndexError (not enough elements in 'words')
             print("IndexError occurred: {}".format(e))
