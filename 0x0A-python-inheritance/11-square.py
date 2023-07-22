bdecb62ad3d7522bb4cc2454214aa65b93ebce3b
mburudev <ianmburu999@gmail.com>
2023-07-22 08:21:59 -0700

    Task 14 correction 6

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -16,8 +16,4 @@ class Square(Rectangle):
         super().__init__(self.__size, self.__size)
 
     def __str__(self):
-        size_str = str(self.__size)
-        max_size_str_length = 245 - len("[Square] ") - 1  # Account for the "[Square] " prefix and the slash in the output
-        if len(size_str) > max_size_str_length:
-            size_str = size_str[:max_size_str_length]
-        return "[Square] " + size_str + "/" + size_str
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
