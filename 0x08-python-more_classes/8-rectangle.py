1f6d4999ef738e83be8fb7a445e27f68e1a670e8
mburudev <ianmburu999@gmail.com>
2023-07-22 09:29:34 -0700

    Task 8 pycodesyle

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -106,14 +106,16 @@ class Rectangle:
             int: The perimeter of the rectangle.
 
         """
-        return 2 * (self._width + self._height) if self._width and self._height else 0
+        return 2 * (self._width + self._height) if self._width and \
+            self._height else 0
 
     def __str__(self):
         """
         Returns a string representation of the rectangle.
 
         If width or height is 0, returns an empty string.
-        Otherwise, returns a string with the rectangle represented by the character(s) in print_symbol.
+        Otherwise, returns a string with the rectangle represented by \
+            the character(s) in print_symbol.
 
         Returns:
             str: String representation of the rectangle.
@@ -121,21 +123,24 @@ class Rectangle:
         """
         if self._width == 0 or self._height == 0:
             return ""
-        return '\n'.join([str(self.print_symbol) * self._width for _ in range(self._height)])
+        return '\n'.join([str(self.print_symbol) * self._width
+                         for _ in range(self._height)])
 
     def __repr__(self):
         """
         Returns a string representation of the rectangle.
 
         Returns:
-            str: String representation of the rectangle that can be used to recreate a new instance.
+            str: String representation of the rectangle that can be \
+                used to recreate a new instance.
 
         """
         return f"Rectangle({self._width}, {self._height})"
 
     def __del__(self):
         """
-        Print a message when an instance of Rectangle is deleted and decrement the number_of_instances.
+        Print a message when an instance of Rectangle is deleted and \
+            decrement the number_of_instances.
 
         """
         print("Bye rectangle...")
@@ -154,12 +159,11 @@ class Rectangle:
             TypeError: If rect_1 or rect_2 is not an instance of Rectangle.
 
         Returns:
-            Rectangle: The rectangle with the bigger area. If both have the same area value, returns rect_1.
-
+            Rectangle: The rectangle with the bigger area. If both have the \
+                same area value, returns rect_1.
         """
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        
         return rect_1 if rect_1.area() >= rect_2.area() else rect_2
