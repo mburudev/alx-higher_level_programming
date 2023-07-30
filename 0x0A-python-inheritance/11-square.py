2d4d9b1a55ac8d74c69df2feeb8f43e9aba943e3
mburudev <ianmburu999@gmail.com>
2023-07-30 09:31:35 -0700

    Task 11 corrections

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -16,8 +16,11 @@ class Square(Rectangle):
     """ Square Class """
     def __init__(self, size):
         """ size init"""
-        self.__size = size
-        super().__init__(self.__size, self.__size)
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size <= 0:
+            raise ValueError("size must be greater than 0")
+        super().__init__(size, size)
 
     def __str__(self):
-        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
+        return "[Square] {}/{}".format(self.width, self.height)
