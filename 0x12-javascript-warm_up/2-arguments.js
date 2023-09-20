d74cf743a09326f791969a0f826c4b115f010970
mburudev <ianmburu999@gmail.com>
2023-09-20 06:04:17 -0700

    Task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const args = process.argv.slice(2);
 const numArgs = args.length;
 if (numArgs === 0) {
-  console.log('No argument found');
+  console.log('No argument');
 } else if (numArgs === 1) {
   console.log('Argument found');
 } else {
