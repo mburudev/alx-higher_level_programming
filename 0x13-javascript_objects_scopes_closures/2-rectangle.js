e68e5c7fa691e2882a09d272c6c7f90cd3f83cdb
mburudev <ianmburu999@gmail.com>
2023-09-27 00:00:44 -0700

    Task 2 corrections

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     } else {
       // If w or h is not a positive integer, create an empty object.
-      this.width = undefined;
-      this.height = undefined;
     }
   }
 };
